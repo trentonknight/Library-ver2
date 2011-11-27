da6b6df01a313ae182faacf8e1ab65eb51626acc
Jason N Mansfield <trentonknight@gmail.com>
2011-11-27 14:26:04 -0500

    authentication test headers.

--- test/library/service/IAuthSvcSerializedImplTest.java
+++ test/library/service/IAuthSvcSerializedImplTest.java
@@ -5,10 +5,6 @@
 package library.service;
 
 import library.domain.Login;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
