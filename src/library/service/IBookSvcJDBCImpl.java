0304e4f3a8de27289428e202183a33345988dbc9
Jason N Mansfield <trentonknight@gmail.com>
2011-11-29 19:27:15 -0500

    finally got stupid try catch to work.

--- src/library/service/IBookSvcJDBCImpl.java
+++ src/library/service/IBookSvcJDBCImpl.java
@@ -5,6 +5,8 @@
 package library.service;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.sql.*;
 import library.domain.Book;
 
@@ -13,9 +15,23 @@ import library.domain.Book;
  * @author trentonknight
  */
 public class IBookSvcJDBCImpl implements IBookSvc {
+    
+    
+   private String url = "jdbc:mysql://localhost/library?" + "user=root&password=admin";
+    
+   private Connection getConnection() throws Exception { 
+      return DriverManager.getConnection(url); 
+   }
+
 
     public Book add(Book book) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            Connection conn = getConnection();
+        } catch (Exception ex) {
+            Logger.getLogger(IBookSvcJDBCImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    return book;
     }
 
     public Book retrieve(Book book) {
